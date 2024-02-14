0668136e48517bebd406d1cc6a4f0aa3f84b9408
Abhishek Makwana <xaeabhishek@gmail.com>
2024-02-15 04:07:26 +0530

    first commit

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Abhishek Makwana </span>
+            from <span className="purple"> Ahmedabad, India.</span>
+            <br />
+            I am currently employed as a software developer Intern at Radical X.
+            <br />
+            I am currently pursuing Bachelors of Engineering (BE) in Information Technology at LJ University
+            Ahmedabad.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Chess !
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Explore Street view on Google Earth
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Read Shonen Mangas
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Human consciousness is a tragic misstep in evolution"{" "}
+          </p>
+          <footer className="blockquote-footer">Abhishek</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
